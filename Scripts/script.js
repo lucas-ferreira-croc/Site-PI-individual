4e2c4b362763642e7f91ac305fd6c601394dc6c6
lucas-ferreira-lf <lucas.ferreira057@bandtec.com.br>
2020-06-02 01:56:56 -0300

    Modifiquei a index, adicionando algumas funcionabilidades visuais

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,53 @@
-function
+//funções da página Index
+function manager()
 {
-    
+    if(gi.style.display == 'none')
+    {
+        gi.style.display = 'block';
+    }
+    else
+    {
+        gi.style.display = 'none';
+    }
 }
+
+function abreLixeira()
+{
+    lixo.style.display = 'block';
+}
+
+function fechaLixeira()
+{
+    lixo.style.display = 'none';
+}
+
+function abrePainel()
+{
+    painel.style.display = 'block';
+}
+
+function fechaPainel()
+{
+    painel.style.display = 'none';
+}
+
+function abreConexao()
+{
+    conexao.style.display = 'block';
+}
+
+function fechaConexao()
+{
+    conexao.style.display = 'none';
+}
+
+function abreInternet()
+{
+    internet.style.display = 'block';
+}
+
+function fechaInternet()
+{
+    internet.style.display = 'none';
+}
+//fim das funções da página Index
